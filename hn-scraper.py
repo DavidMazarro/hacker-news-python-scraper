6f52c4ae0bbd7f15bb7864354ed1b42502fbe189
DavidMazarro <davidmazarro98@gmail.com>
2021-10-25 14:27:37 +0200

    style: main script now prints which group is which

--- hn-scraper.py
+++ hn-scraper.py
@@ -7,8 +7,10 @@ entries: list[Entry] = scrape("https://news.ycombinator.com/")
 entries1 = entries_gr5_comment_num(entries)
 entries2 = entries_leq5_points(entries)
 
+print("-- Entries with more than 5 words in title, sorted by num. of comments--\n")
 for entry in entries1:
     print(entry)
 print('\n')
+print("-- Entries with less than 5 words or equal in title, sorted by points--\n")
 for entry in entries2:
     print(entry)
